469af9b7a099f3ca73b03ad66c24f2c8502eb81d
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-12-20 21:49:48 +0530

    Motion1Activity added.

--- app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion1Activity.kt
+++ app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion1Activity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Motion1Activity : AppCompatActivity() {
 
@@ -16,5 +17,13 @@ class Motion1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion1)
+
+        findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
+
+            Motion2Activity.start(
+                this
+            )
+
+        }
     }
 }
