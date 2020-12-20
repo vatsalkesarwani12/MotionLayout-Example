82fbac1fe88ecc4e7762444c49b45d1e3c6faeea
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-12-21 02:47:16 +0530

    motion2 added.

--- app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion2Activity.kt
+++ app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion2Activity.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View.inflate
+import android.widget.ImageView
+import android.widget.Toolbar
 
 class Motion2Activity : AppCompatActivity() {
 
@@ -16,5 +22,26 @@ class Motion2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion2)
+        setSupportActionBar(findViewById(R.id.toolbar1))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<ImageView>(R.id.imageView2).setOnClickListener{
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        if (item.itemId == R.id.imageView) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
