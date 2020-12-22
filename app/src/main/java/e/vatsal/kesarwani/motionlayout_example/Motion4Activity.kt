d58794b1d7ae7a0354d5e910d26476b853099003
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-12-22 23:51:26 +0530

    motion4 added with multiple keyPostion attribute

--- app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion4Activity.kt
+++ app/src/main/java/e/vatsal/kesarwani/motionlayout_example/Motion4Activity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Motion4Activity : AppCompatActivity() {
 
@@ -16,5 +17,11 @@ class Motion4Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion4)
+
+        findViewById<FloatingActionButton>(R.id.floatingActionButton4).setOnClickListener {
+            Motion5Activity.start(
+                    this
+            )
+        }
     }
 }
